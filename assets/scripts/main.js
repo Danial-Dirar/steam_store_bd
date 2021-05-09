da7e0d7e29f6a946d870a53a4e03d7e9f1f25c33
Ahtemarul Hasan <ahtemarulhasanrokon@gmail.com>
2021-05-09 13:53:14 +0600

    The landing page is finished.
    The Landing page is finished. animations are not implemented

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+console.log("Like Our Facebook Page ====> https://www.facebook.com/steamstorebd");
+console.log("join Our Steam Group ====> (on hold currently)");
+console.log("join Our Discord Server ====> https://discord.gg/pRAehuvJ3U");
+console.log("The Site is not yet fully ready.");
+console.log("This is only to show what is available.");
+console.log("So please contact the page owner to actually buy/trade.");
+console.log("Have a good day, sir.");
---
+++ readme.md
@@ -0,0 +1,4 @@
+*DANIEL BOYEEE*
+fonts = https://fonts.google.com/specimen/Source+Sans+Pro
+
+ki likhbo jani na :(
